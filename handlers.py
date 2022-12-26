5c57778208477d8a3129744a77fae25cc39492a2
Yuri Karzhov <liralaysometimes@yandex.ru>
2022-12-27 01:14:04 +0300

    1st Commit

--- handlers.py
+++ handlers.py
@@ -6,13 +6,14 @@ from config import adminID
 async def send_to_admin(dp):
     await bot.send_message(chat_id = adminID, text = "Bot launched") 
 
+@dp.message_handler(commands=["start"])
+async def start_message(message: Message):
+    await message.answer("Привет. Этот бот поможет тебе найти картинки по твоему запросу \n Напиши любое слово и он найдет подходящую картинку")
+
 @dp.message_handler()
 async def echo(message: Message):
-    if message.text == "/start":
-        await message.answer("Привет. Этот бот поможет тебе найти картинки по твоему запросу \n Напиши любое слово и он найдет подходящую картинку")
-    else:
-        user_query = message.text         #имеется запрос
-        blok_list = user_query.split()                        #разбиваем слова по пробелам
-        url_query = '%20'.join(blok_list)                     #разделяем их через %20
-        text = f"https://yandex.ru/images/search?text={url_query}&lr=213"
-        await message.answer(text=text)
+    user_query = message.text         #имеется запрос
+    blok_list = user_query.split()                        #разбиваем слова по пробелам
+    url_query = '%20'.join(blok_list)                     #разделяем их через %20
+    text = f"https://yandex.ru/images/search?text={url_query}&lr=213"
+    await message.answer(text=text)
